856e1307298d37802d5916c881e2c043f20567bf
farukci <ferruhcihan20@gmail.com>
2020-09-25 13:10:26 +0300

    project updated

--- digital.js
+++ digital.js
@@ -7,7 +7,12 @@ setInterval(() => {
     let h = new Date().getHours();
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
-    let am = "AM";
+    var am = "AM";
+
+    if(h > 12){
+        h = h - 12;
+        var am = "PM"
+    }
 
     h = (h<10) ? "0"+h : h;
     m = (m<10) ? "0"+m : m;
@@ -17,11 +22,4 @@ setInterval(() => {
     minutes.innerHTML = m+":";
     seconds.innerHTML = s;
     ampm.innerHTML = am;
-
-    if(h<12){
-        h = h - 12;
-        let am = "PM"
-    }
-
-
 })
